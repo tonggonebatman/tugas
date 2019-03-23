eb5836b5a51f349b1e2a0f702e747ab5a2ff0ed3
tonggonebatman <48628747+tonggonebatman@users.noreply.github.com>
2019-03-23 05:16:55 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>information retrival unisbank</title>
+		<style>
+				h1{
+					color:black;
+					text-align:center;
+					margin-top:10%;
+					
+					}
+		</style>
+	</head>
+			<body>
+				<h1> <?php echo 'hello world';?> </h1>
+			</body>
+</html>
